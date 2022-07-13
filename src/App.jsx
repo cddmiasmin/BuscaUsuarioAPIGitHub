1dc9c221540a218a4eabb0ab8c7bfa5be14faf9c
Iasmin Dias <iasmin.dias87@gmail.com>
2022-07-13 18:41:24 -0300

    :wave: Hello World! + :adult: Componente Cabecalho

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Cabecalho from './componentes/Cabecalho/Cabecalho'
+
+function App() {
+  return (
+    <div className="App">
+      <Cabecalho/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/componentes/Cabecalho/Cabecalho.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const Cabecalho = () => {
+  return (
+    <div className='cabecalho'>
+        <img id="logo" src="https://www.svgrepo.com/show/353783/github-octocat.svg" alt="Logo Octocat GitHub" />
+
+        <h1>GitHub</h1>
+    </div>
+  )
+}
+
+export default Cabecalho
